afe479ddfb8c518e07862fd2cc5617fdc5e39e78
welociraptor <17182067+welociraptor@users.noreply.github.com>
2025-04-05 19:51:56 +0300

    Start moving functionality to Cinc recipes

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,27 +1,7 @@
-#!/usr/bin/bash
+#!/bin/sh -x
 
-# Fish shell
-sudo apt install --yes fish
-
-# Proto
-
-# Required dependencies..
-sudo apt install --yes git unzip gzip xz-utils
-
-# Install script
-bash <(curl -fsSL https://moonrepo.dev/install/proto.sh) --no-profile --yes
-
-# Set path and activate on shell startup
-fish -c fish_add_path ~/.proto/bin/
-echo "proto activate fish | source" >> ~/.config/conf.d/proto.fish
-cp prototools ~/.proto/.prototools
-
-# Cinc workstation
+# Bootstrap script to install Cinc workstation which will be used for provisioning
 
 curl -O https://downloads.cinc.sh/files/stable/cinc-workstation/25.2.1075/debian/12/cinc-workstation_25.2.1075-1_amd64.deb
 sudo dpkg -i cinc-workstation_25.2.1075-1_amd64.deb
 rm cinc-workstation_25.2.1075-1_amd64.deb
-
-# Finish bootstrapping with running apt upgrade
-
-sudo apt ugprade
--- prototools.rb
+++
@@ -1,3 +0,0 @@
-file "/home/welociraptor.linux/.proto/.prototools" do
-    content IO.read('prototools')
-end
